da0aa68cc2b583693f93bbd502f8a3f3962aca2d
Tadeáš Anděl <tadeas.andel@gmail.com>
2019-12-29 00:12:51 +0100

    Movable objects implemented, Platform trigger prepared

--- Source/PuzzleMuzzle/MovingPlatform.cpp
+++ Source/PuzzleMuzzle/MovingPlatform.cpp
@@ -1,5 +1,46 @@
 // Fill out your copyright notice in the Description page of Project Settings.
 
-
 #include "MovingPlatform.h"
 
+AMovingPlatform::AMovingPlatform()
+{
+  PrimaryActorTick.bCanEverTick = true;
+
+  SetMobility(EComponentMobility::Movable);
+}
+
+void AMovingPlatform::BeginPlay()
+{
+  Super::BeginPlay();
+
+  if (HasAuthority())
+  {
+    SetReplicates(true);
+    SetReplicateMovement(true);
+  }
+  GlobalTargetLocation = GetTransform().TransformPosition(TargetLocation);
+  GlobalStartLocation = GetTransform().TransformPosition(StartLocation);
+}
+
+void AMovingPlatform::Tick(float DeltaTime)
+{
+  Super::Tick(DeltaTime);
+
+  if (HasAuthority())
+  {
+    FVector Location = GetActorLocation();
+    float JourneyLength = (GlobalTargetLocation - GlobalStartLocation).Size();
+    float JourneyTraveled = (Location - GlobalStartLocation).Size();
+
+    if (JourneyTraveled > JourneyLength)
+    {
+      FVector Swap = GlobalStartLocation;
+      GlobalStartLocation = GlobalTargetLocation;
+      GlobalTargetLocation = Swap;
+    }
+
+    FVector Direction = (GlobalTargetLocation - GlobalStartLocation).GetSafeNormal();
+    Location += Speed * DeltaTime * Direction;
+    SetActorLocation(Location);
+  }
+}
--- Source/PuzzleMuzzle/MovingPlatform.h
+++ Source/PuzzleMuzzle/MovingPlatform.h
@@ -13,5 +13,23 @@ UCLASS()
 class PUZZLEMUZZLE_API AMovingPlatform : public AStaticMeshActor
 {
 	GENERATED_BODY()
-	
+
+public:
+	AMovingPlatform();
+
+	virtual void BeginPlay() override;
+
+	virtual void Tick(float DeltaTime) override;
+
+	UPROPERTY(EditAnywhere)
+	float Speed = 20;
+
+	UPROPERTY(EditAnywhere, Meta = (MakeEditWidget = true))
+	FVector TargetLocation;
+	UPROPERTY(EditAnywhere, Meta = (MakeEditWidget = true))
+	FVector StartLocation;
+
+private:
+	FVector GlobalTargetLocation;
+	FVector GlobalStartLocation;
 };
