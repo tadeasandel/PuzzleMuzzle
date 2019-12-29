e9ee0d05c72a7f8ec631206a5d0661adcf348afb
Tadeáš Anděl <tadeas.andel@gmail.com>
2019-12-29 23:39:37 +0100

    Bug fixed!

--- Source/PuzzleMuzzle/PlatformTrigger.h
+++ Source/PuzzleMuzzle/PlatformTrigger.h
@@ -26,4 +26,10 @@ public:
 private:
 	UPROPERTY(VisibleAnywhere)
 	class UBoxComponent *TriggerVolume;
+
+	UFUNCTION()
+	void OnOverlapBegin(UPrimitiveComponent *OverlappedComp, AActor *OtherActor, UPrimitiveComponent *OtherComp, int32 OtherBodyIndex, bool bFromSweep, const FHitResult &SweepResult);
+
+	UFUNCTION()
+	void OnOverlapEnd(UPrimitiveComponent *OverlappedComp, AActor *OtherActor, UPrimitiveComponent *OtherComp, int32 OtherBodyIndex);
 };
